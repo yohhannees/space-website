b4d88dcbf145b0de1e27166954623227f18de374
Yohannees <yohannesgetachewerieso@gmail.com>
2024-03-23 02:28:12 +0300

    faq page and about page basic ui completed

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,37 +1,83 @@
-'use client';
-import React from "react";
-import { Form, Input } from "antd";
+"use client";
+import { useState } from "react";
+import { Container, Title } from "@mantine/core";
 
-function ContactUs() {
+const placeholder =
+  "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.";
+
+function FaqSimple() {
+  const [openSection, setOpenSection] = useState<string | null>(null);
+
+  const toggleSection = (section: string) => {
+    setOpenSection(openSection === section ? null : section);
+  };
+
   return (
-    
-    <div className="flex items-center justify-center pt-11 pb-72 ">
-      <div className="p-6 max-w-sm w-full bg-inherit bg-opacity-0 border border-white">
-        <h1 className="text-center mb-4 text-white">Contact Us</h1>
-        <Form>
-          <Form.Item
-            
-            name="name"
-            rules={[{ required: true, message: "Please input your name!" }]}
-          >
-            <Input placeholder="Name" />
-          </Form.Item>
-          <Form.Item
-            name="email"
-            rules={[{ required: true, message: "Please input your email!" }]}
-          >
-            <Input placeholder="Email" />
-          </Form.Item>
-          <Form.Item
-            name="text"
-            rules={[{ required: true, message: "Please input your Message!" }]}
-          >
-            <Input.TextArea placeholder="Text" rows={4} />
-          </Form.Item>
-        </Form>
+    <Container size="sm" className="text-center text-white flex justify-center">
+      <div className="max-w-md">
+        <Title ta="center" className="m-3 text-3xl mb-4">
+          Frequently Asked Questions
+        </Title>
+
+        <div className="border border-white rounded-md p-4">
+          <div className="mb-4" onClick={() => toggleSection("password")}>
+            <div className=" border-white rounded-md p-4">
+              <h3 className="cursor-pointer font-bold">
+                How can I reset my password?
+              </h3>
+              {openSection === "password" && (
+                <p className="text-white">{placeholder}</p>
+              )}
+            </div>
+          </div>
+
+          <div className="mb-4" onClick={() => toggleSection("account")}>
+            <div className="border-white rounded-md p-4">
+              <h3 className="cursor-pointer  font-bold">
+                Can I create more than one account?
+              </h3>
+              {openSection === "account" && (
+                <p className="text-white">{placeholder}</p>
+              )}
+            </div>
+          </div>
+
+          <div className="mb-4" onClick={() => toggleSection("newsletter")}>
+            <div className=" rounded-md p-4">
+              <h3 className="cursor-pointer  font-bold">
+                How can I subscribe to monthly newsletter?
+              </h3>
+              {openSection === "newsletter" && (
+                <p className="text-white">{placeholder}</p>
+              )}
+            </div>
+          </div>
+
+          <div className="mb-4" onClick={() => toggleSection("creditCard")}>
+            <div className=" rounded-md p-4">
+              <h3 className="cursor-pointer  font-bold">
+                Do you store credit card information securely?
+              </h3>
+              {openSection === "creditCard" && (
+                <p className="text-white">{placeholder}</p>
+              )}
+            </div>
+          </div>
+
+          <div className="mb-4" onClick={() => toggleSection("payment")}>
+            <div className="border-white rounded-md p-4">
+              <h3 className="cursor-pointer  font-bold">
+                What payment systems do you work with?
+              </h3>
+              {openSection === "payment" && (
+                <p className="text-white">{placeholder}</p>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
 
-export default ContactUs;
+export default FaqSimple;
