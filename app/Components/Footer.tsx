7c6f2c0fe2383aa27302a602fb87245bfa3e9279
yohhannees <yohannesgetachewerieso@gmail.com>
2024-04-12 01:16:08 +0300

    updated

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,11 +1,14 @@
-'use client'
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-import Image from 'next/image';
+"use client";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+import Image from "next/image";
 
 const Footer = () => {
-
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
   useEffect(() => {
@@ -19,55 +22,72 @@ const Footer = () => {
         <div className=" pt-[300px] md:pt-0 mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className=" md:flex md:justify-between">
             <div className="hidden md:block relative mb-6 md:mb-0">
-              <Image src="/assets/image.png"  alt="Logo" width={200} height={200} className="me-3" />
-              <div className="absolute top-20 left-16 text-white text-center flex items-center justify-center ">
+              <div className="absolute -bottom-6 left-32  h-32 w-32 rounded-full bg-[#47C2C2]"></div>
+              <div className="absolute top-8 -left-3 text-white text-center h-52 w-52  rounded-full bg-[#6480ff] flex items-center justify-center ">
                 GET IN TOUCH
               </div>
-              <div className="absolute -bottom-6 left-32  h-32 w-32 rounded-full bg-[#47C2C2]"></div>
-              <div className="absolute -top-6 -left-16  h-32 w-32 rounded-full bg-[#1717174b]"></div>
+              <div className="absolute -top-6 -left-16  h-32 w-32 rounded-full bg-blend-lighten bg-black">
+                {" "}
+                <Image
+                  src="/pictures/ahamenesLogo.png"
+                  alt="Logo"
+                  width={200}
+                  height={200}
+                  className="me-3"
+                />
+              </div>
             </div>
 
             <div className="flex flex-col gap-2 ">
               <div className="flex items-center justify-start">
-               <Image src="/pictures/ahamenesLogo.png" alt="Logo" width={80} height={80} className="w-20" />
+                <Image
+                  src="/pictures/ahamenesLogo.png"
+                  alt="Logo"
+                  width={80}
+                  height={80}
+                  className="w-20"
+                />
                 <div className=" text-white font-bold text-3xl font-sans">
-                  <span className='tracking-widest'>
-                  AHAMENES
-                  </span>
+                  <span className="tracking-widest">AHAMENES</span>
                 </div>
               </div>
 
               <div className="grid grid-cols-2 md:grid-cols-2 gap-10 text-xs sm:gap-20 sm:grid-cols-2">
                 <div>
-                  <h2 className="font-sans mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                  <h2 className="font-sans mb-6 text-sm font-semibold text-slate-400 uppercase dark:text-white">
                     Website Links
                   </h2>
                   <ul className="font-sans text-gray-500 dark:text-gray-400 font-medium">
                     <li className="mb-4">
                       <a
                         href="https://www.w3.org/Provider/Style/dummy.html"
-                        className="hover:underline">
+                        className="hover:underline"
+                      >
                         Home
                       </a>
                     </li>
                     <li className="mb-4">
                       <a
                         href="https://www.w3.org/Provider/Style/dummy.html"
-                        className="hover:underline">
+                        className="hover:underline"
+                      >
                         About
                       </a>
                     </li>
-                    <li className="mb-4">
+                    <li className="mb-4 text-slate-400">
                       <a
                         href="https://www.w3.org/Provider/Style/dummy.html"
-                        className="font-sans hover:underline">
-                        Services
+                        className="font-sans hover:underline"
+                      >
+                        <div className=" text-slate-400">Services</div>
+                        
                       </a>
                     </li>
                     <li>
                       <a
                         href="https://www.w3.org/Provider/Style/dummy.html"
-                        className="font-sans hover:underline">
+                        className="font-sans hover:underline"
+                      >
                         FAQs
                       </a>
                     </li>
@@ -82,28 +102,32 @@ const Footer = () => {
                     <li className="mb-4">
                       <a
                         href="https://www.w3.org/Provider/Style/dummy.html"
-                        className="hover:underline">
+                        className="hover:underline"
+                      >
                         Architecture
                       </a>
                     </li>
                     <li className="mb-4">
                       <a
                         href="https://www.w3.org/Provider/Style/dummy.html"
-                        className="hover:underline">
+                        className="hover:underline"
+                      >
                         Buildings{" "}
                       </a>
                     </li>
                     <li className="mb-4">
                       <a
                         href="https://www.w3.org/Provider/Style/dummy.html"
-                        className="hover:underline">
+                        className="hover:underline"
+                      >
                         3d maps
                       </a>
                     </li>
                     <li>
                       <a
                         href="https://www.w3.org/Provider/Style/dummy.html"
-                        className="hover:underline">
+                        className="hover:underline"
+                      >
                         Structure Design
                       </a>
                     </li>
@@ -116,21 +140,24 @@ const Footer = () => {
           <div className="w-full flex items-center justify-between">
             {" "}
             <div className="flex justify-start ">
-            <a
-  href="https://www.w3.org/Provider/Style/dummy.html"
-  className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-  <FontAwesomeIcon icon={faFacebookF} />
-</a>
-<a
-  href="https://www.w3.org/Provider/Style/dummy.html"
-  className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-  <FontAwesomeIcon icon={faInstagram} />
-</a>
-<a
-  href="https://www.w3.org/Provider/Style/dummy.html"
-  className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-  <FontAwesomeIcon icon={faLinkedinIn} />
-</a>
+              <a
+                href="https://www.w3.org/Provider/Style/dummy.html"
+                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+              >
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+              <a
+                href="https://www.w3.org/Provider/Style/dummy.html"
+                className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+              >
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+              <a
+                href="https://www.w3.org/Provider/Style/dummy.html"
+                className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+              >
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </a>
             </div>
             <div className="flex   justify-center ">
               <span className="text-sm  text-white sm:text-center ">
--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 function Navbar() {
-  const [showMenu, setShowMenu] = useState(false);
-
   return (
     <div className="lg:h-[80px] bg-transparent flex justify-between items-center px-16 text-white">
       <div className="mt-5">
@@ -24,34 +22,11 @@ function Navbar() {
         <a href="/team">TEAM</a>
         <a href="/contact">CONTACT US</a>
         <a href="/faq">FAQ</a>
-        {/* <div
-          className="relative "
-          onMouseEnter={() => setShowMenu(true)}
-          onMouseLeave={() => setShowMenu(false)}
-        >
-          <button className="cursor-pointer">CLUB WEBSITE</button>
-          {showMenu && (
-            <div className="absolute top-full left-0 p-2  border border-gray-300 shadow-md">
-              <div className=" border border-gray-300">
-               
-                <a href="#" className="block py-2 px-3">
-                  Space explorer
-                </a>
-              </div>
-              <div className=" border border-gray-300">
-                <a href="#" className="block py-2 px-3">
-                  Space Wiki
-                </a>
-              </div>
-              <div className=" border border-gray-300"></div>
-            </div>
-          )}
-        </div> */}
         <a href="/gallery">GALLERY & MEDIA</a>
       </div>
       <div>
         <button className="bg-red-500 px-5 py-1 text-white text-sm rounded-full ">
-          Explore
+          Space Wiki
         </button>
       </div>
     </div>
